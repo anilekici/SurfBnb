3458a24d600093af50a893802c11e000085867e1
Ardeshir Gourtani <ardeshir_mostajeran@hotmail.co.uk>
2020-11-10 14:39:19 +0000

    done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user, :surfboard
 
-  validates :date_from, :date_to, presence: true
-
-  validates
+  validates :date_from, :date_to, :surfboard, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :surfboards, except: %i[edit update]
+  resources :surfboards, except: %i[edit update] do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :bookings, only: [:new, :create]
+  end
+  resources :booking, only: :destroy
 end
