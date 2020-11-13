d713119adb6b3e07096abadde88454b898ba259c
TJ Hollis <thewelshaussie@hotmail.com>
2020-11-13 11:11:14 +0000

    migration refactor

--- db/migrate/20201110105434_create_bookings.rb
+++ db/migrate/20201110105434_create_bookings.rb
@@ -1,6 +1,10 @@
 class CreateBookings < ActiveRecord::Migration[6.0]
   def change
     create_table :bookings do |t|
+      t.date :start_date
+      t.date :end_date
+      t.references :user, null: false, foreign_key: true
+      t.references :surfboard, null: false, foreign_key: true
 
       t.timestamps
     end
